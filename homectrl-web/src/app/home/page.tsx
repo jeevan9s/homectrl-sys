1e2b09c397c0d176109fb5b11b56c1feb6db238a
jeevansanchez <166963895+jeevansanchez@users.noreply.github.com>
2025-08-12 20:44:44 -0400

    s

---
+++ homectrl-web/src/app/home/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div id="page" className="font-sans items-center justify-items-center min-h-screen">
+      <div id="m-header-cntr">
+        <h1 id="main-header">homectrl</h1>
+      </div>
+
+      <div id="s-header-cntr">
+        <h2 id="subheading">welcome</h2>
+      </div>
+    </div>
+  )
+}
--- homectrl-web/src/app/page.tsx
+++ homectrl-web/src/app/page.tsx
@@ -1,9 +1,67 @@
+"use client"
+import { useEffect, useState } from "react"
+import Home from "./home/page"
+  const messages = [
+    "initializing modules",
+    "syncing devices",
+    "almost set"
+  ]
+
+export default function Landing() {
+  const [count, setCount] = useState(0)
+  const [intervalTime, setIntervalTime] = useState(100)
+  const [messageIndex, setMessageIndex] = useState(0)
+
+  useEffect(()=> {
+    
+
+    const msgInterval = setInterval(() => {
+      setMessageIndex((prev) => (prev + 1) % messages.length)
+    }, 800)
+
+    return () => clearInterval(msgInterval) 
+  },[])
+
+  const incrementVal = (currCount: number): number => {
+    if (currCount < 20) return 4.5
+    if (currCount < 40) return 3
+    if (currCount < 60) return 5
+    if (currCount < 75) return 9
+    if (currCount < 90) return 7
+    return 3
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(prev => {
+        if (prev >= 100) return 100
+
+        const increment = incrementVal(prev)
+        const newCount = Math.min(prev + increment, 100)
+
+        
+        if (newCount < 20) setIntervalTime(100)
+        else if (newCount < 50) setIntervalTime(92)
+        else if (newCount < 75) setIntervalTime(82)
+        else setIntervalTime(75)
+
+        return newCount
+      })
+    }, intervalTime)
+
+    return () => clearInterval(interval)
+  }, [intervalTime])
+  if (count >= 100) return <Home />
 
 
-export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1>homectrl</h1>
-    </div>
-  );
+return (
+  <div className="font-sans flex flex-col items-center justify-center min-h-screen z-1 p-8 text-center lg:-translate-y-10">
+
+    <h1 className="select-none text-white text-xl">welcome to homectrl.</h1>
+    <h1 id="count" className="text-lg text-white/50 mt-10">
+      {Math.round(count)}%
+    </h1> 
+    <p className="text-md text-white/30">{messages[messageIndex]}</p>
+  </div>
+)
 }
