49ad22dda5c3b37c249a0cdce22b9cd687dfae67
Jeevan Sanchez <jeevansanchez42@gmail.com>
2026-01-02 21:35:25 -0500

    layout work

---
+++ main.py
@@ -0,0 +1,9 @@
+# when my tip gets sticky > jeevan: bend over and ;let Noori spank you 
+
+isTipSticky = input("is the tip sticky?")
+
+if isTipSticky == "yes":
+    print("noori bends over")
+
+elif isTipSticky == "no":
+    print("noori gets jeevan's tip sticky")
