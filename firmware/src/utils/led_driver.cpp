85539092aa3527440d0357c85247ecb8b86250dd
Jeevan Sanchez <jeevansanchez42@gmail.com>
2026-06-11 23:12:30 -0400

    added pinouts

---
+++ firmware/src/utils/led_driver.cpp
@@ -0,0 +1,9 @@
+/* Drivers for onboard LEDs 
+ * Jeevan Sanchez
+ *
+ * Created: Feb.19.2026
+ * Updated: May.31.2026
+*/
+
+#include "led_driver.h"
+
---
+++ firmware/src/utils/led_driver.h
@@ -0,0 +1,9 @@
+#ifndef LED_DRIVER_H
+#define LED_DRIVER_H 
+
+#include "../pinouts.h"
+#include <Adafruit_NeoPixel.h>
+
+
+
+#endif 
