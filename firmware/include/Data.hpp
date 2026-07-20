52202bb605590ee84b7d9e233986fb6384ea80ed
Jeevan Sanchez <jeevansanchez42@gmail.com>
2026-07-20 13:47:34 -0400

    implement  bidirectional serialization layer

--- firmware/include/Data.hpp
+++ firmware/include/Data.hpp
@@ -1,28 +1,52 @@
 /**
  * @file        Data.hpp
- * @brief       Controller peripheral data object definition. 
+ * @brief       Controller peripheral data object definition.
  * @author      Jeevan Sanchez
  * @date        2026-07-18
  *
  * HOMECTRL
  */
 
-struct PumpData {
-    /// 
-}; 
-
-struct StepMtrData {
-    /// 
-}; 
-
-struct SMSData {
-    /// 
+struct PumpData
+{
+    ActuatorState state;
+    int enable;
 };
 
-struct ALSData {
-    /// 
-}; 
+struct StepMtrData
+{
+    ActuatorState state;
+    int enable; 
+    int shadePct; 
+};
 
-struct FSWData {
-    /// 
-}; 
+struct SMSData
+{
+    int32_t raw;
+    float moisture;
+    bool hydrated;
+};
+
+// struct ALSData {
+//     ///
+// };
+
+struct FSWData
+{
+    boolean empty;
+};
+
+enum struct ActuatorState
+{
+    IDLE,
+    ENERGIZING,
+    RUNNING,
+    FAULT
+};
+
+enum struct DashboardCommands {
+    IRRIGATE_1 = 0,
+    IRRIGATE_2 = 1, 
+    SHADE_DEFAULT = 2, 
+    SHADE_PCT = 3
+};
--- firmware/src/network/telemetry.cpp
+++ firmware/src/network/telemetry.cpp
@@ -12,4 +12,27 @@
 
 void Telemetry::serialize(const Controller &state, JsonDocument &doc) {
     doc["timestamp"] = millis(); 
+
+    JsonObject pump1 = doc["PUMP1"].to<JsonObject>(); 
+    JsonObject pump2 = doc["PUMP1"].to<JsonObject>(); 
+    JsonObject stepper = doc["STEP"].to<JsonObject>();
+    JsonObject sms1 = doc["SMS1"].to<JsonObject>(); 
+    JsonObject sms2 = doc["SMS2"].to<JsonObject>(); 
+    // JsonObject als = doc["ALS1"].to<JsonObject>(); 
+    JsonObject fsw = doc["FSW"].to<JsonObject>(); 
+
+    pump1["state"] = state.pump1.state; 
+    pump2["state"] = state.pump2.state; 
+
+    stepper["state"] = state.stepMotor.state; 
+
+    sms1["raw"] = state.sms1.raw; 
+    sms1["moisture"] = state.sms1.moisture; 
+    sms1["isHydrated"] = state.sms1.hydrated; 
+    
+    sms2["raw"] = state.sms2.raw; 
+    sms2["moisture"] = state.sms2.moisture; 
+    sms2["isHydrated"] = state.sms2.hydrated; 
+
+    fsw["tankStatus"] = state.fsw.empty;
 }
